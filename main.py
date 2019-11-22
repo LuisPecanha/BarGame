4a2641a249a43600c0402006d01a940a86815eb5
luispecanha <luisgustavopxv@hotmail.com>
2019-11-22 04:13:07 -0300

    Implemented upgrades to printInterface function

--- main.py
+++ main.py
@@ -10,19 +10,12 @@ names = ['João', 'Mario', 'Isabela', 'Kalinka', 'Kamila', 'Paçoca', 'Tiburcio'
          'Mil Grau', 'Rastelo', 'Boiadeiro', 'Raj', 'Papai', 'Linga', 'Guaxa', 'Sumido', 'Maome', 'GPS', 'Marina',
          'Kristrun', 'Bakugou', 'Todoroki', 'PeidaFogo', 'Naruto', 'Paloma', 'Giulia', 'Laura', 'Mavis', 'Jessica']
 
-# Format = Name, Prepare time, Coins Awarded
-beverages = ( ('Caju', '10', '5',),
-              ('Corote', '15', '10'),
-              ('Suco de Bagre', '30', '20'), 
-              ('Margarita', '60', '35'), 
-              ('Piña Colada', '100', '60') )
-
-# Format = Level, Cost to Upgrade
-beveragesStatus = [ [1, 20],
-                   [1, 60],
-                   [1, 120],
-                   [1, 240],
-                   [1, 400] ]
+# Format = Name, Prepare time, Coins Awarded, Beverage Level, Cost to Upgrade
+beverages = [ ['Caju', 10, 5, 1, 20],
+              ['Corote', 15, 10, 1, 60],
+              ['Suco de Bagre', 30, 20, 1, 120], 
+              ['Margarita', 60, 35, 1, 240], 
+              ['Piña Colada', 100, 60, 1, 400] ]
 
 def beverageHandler():
     print()
@@ -42,8 +35,8 @@ class Bar:
         print('Bar initilized with {0} seats'.format(numberOfSeats))
         print('Customer min interval {0}'.format(customerIntervalMin))
         print('Customer max interval {0}'.format(customerIntervalMax))
-        print('Haircut min duration {0}'.format(preparoBebidaMin))
-        print('Haircut max duration {0}'.format(preparoBebidaMax))
+        print('Beverage preparation min duration {0}'.format(preparoBebidaMin))
+        print('Beverage preparation max duration {0}'.format(preparoBebidaMax))
         print('---------------------------------------')
 
     def openShop(self):
@@ -83,8 +76,8 @@ class Bar:
 class Beverage:
     def __init__(self, beverageIndex):
         self.beverageName = beverages[beverageIndex][0]
-        self.beveragePrepareTime = int(beverages[beverageIndex][1]) / beveragesStatus[beverageIndex][0]
-        self.beverageCost = int(beverages[beverageIndex][2]) * beveragesStatus[beverageIndex][1]
+        self.beveragePrepareTime = beverages[beverageIndex][1]
+        self.beverageCost = beverages[beverageIndex][2]
 
 class Customer:
     def __init__(self, beveragesLevel):
@@ -109,18 +102,21 @@ class BarTender:
         time.sleep(beveragePreparationTime)
         print('{0} is done'.format(customer.name))
 
-def printInterface(beveragesLevel):
+def printInterface(beveragesLevel, cash):
     print("Available drinks:")
     if (beveragesLevel == 0):
-        print("{0}.{1}\t Prepare Time: {2} Price: {3} Level: {4} Upgrade Cost: {5}".format(beveragesLevel+1, beverages[beveragesLevel][0], beverages[beveragesLevel][1], beverages[beveragesLevel][2], beveragesStatus[beveragesLevel][0],  beveragesStatus[beveragesLevel][1]))
+        print("{0}.{1}\t Prepare Time: {2} Price: {3} Level: {4} Upgrade Cost: {5}".format(beveragesLevel+1, beverages[0][0], beverages[0][1], beverages[0][2], beverages[0][3],  beverages[0][4]))
     else:
-        print("{0}.{1}\t\t Prepare Time: {2} Price: {3} Level: {4} Upgrade Cost: {5}".format(1, beverages[0][0], beverages[0][1], beverages[0][2], beveragesStatus[0][0], beveragesStatus[0][1]))        
-        for i in range(1 , beveragesLevel):
-            print("{0}.{1}\t Prepare Time: {2} Price: {3} Level: {4} Upgrade Cost: {5}".format(i+1, beverages[i][0], beverages[i][1], beverages[i][2], beveragesStatus[i][0], beveragesStatus[i][1]))
+        print("{0}.{1}\t\t Prepare Time: {2} Price:  {3} Level: {4} Upgrade Cost: {5}".format(1, beverages[0][0], beverages[0][1], beverages[0][2], beverages[0][3], beverages[0][4]))        
+        for i in range(1 , beveragesLevel+1):
+            print("{0}.{1}\t Prepare Time: {2} Price: {3} Level: {4} Upgrade Cost: {5}".format(i+1, beverages[i][0], beverages[i][1], beverages[i][2], beverages[i][3], beverages[i][4]))
+
+    print("Cash gained until now: {0}", cash)
 
 if __name__ == '__main__':
 
     beveragesLevel = 4  # Variable to track level of bevarages of bar and know which ones are available
+    cash = 0
 
     customerIntervalMin = 3     # 3 seconds
     customerIntervalMax = 15    # 15 seconds
@@ -148,7 +144,7 @@ if __name__ == '__main__':
     bartender = BarTender()
 
     bar = Bar(bartender, customerIntervalMin, customerIntervalMax, numberOfSeats=3)
-    printInterface(beveragesLevel)
+    printInterface(beveragesLevel, cash)
     bar.openShop()
 
     while len(customers) > 0:
